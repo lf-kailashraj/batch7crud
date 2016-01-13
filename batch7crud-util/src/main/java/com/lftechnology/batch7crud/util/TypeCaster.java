2eb5bc5ec6fb72f422ffb634b793d22056841359
Kailash Bijayananda <kailashraj@lftechnology.com>
2016-01-13 13:09:58 +0545

    first commit

---
+++ batch7crud-util/src/main/java/com/lftechnology/batch7crud/util/TypeCaster.java
@@ -0,0 +1,29 @@
+package com.lftechnology.batch7crud.util;
+
+import java.math.BigDecimal;
+
+public class TypeCaster {
+    public static Integer toInt(String value) {
+        try {
+            return Integer.valueOf(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static Long toLong(String value) {
+        try {
+            return Long.valueOf(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static BigDecimal toBigDecimal(String value) {
+        try {
+            return new BigDecimal(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+}
