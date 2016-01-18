3b87a3e3e9625d11fef09d4c4aa918acc912bdf0
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-18 17:51:41 +0545

    added create method in UserDao

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/dao/UserDao.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/dao/UserDao.java
@@ -1,7 +1,28 @@
 package com.lftechnology.batch7crud.dao;
 
+import com.lftechnology.batch7crud.db.DBConnection;
+import com.lftechnology.batch7crud.exception.DataException;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by grishma on 1/18/16.
  */
 public class UserDao {
+    public void create(String name, String username, String email, String password) throws DataException {
+        try {
+            String sql = "INSERT INTO USERS (name, username, email, password) VALUES (name, username, email, password)";
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+        }
+        catch (SQLException e) {
+            throw new DataException();
+        }
+
+    }
 }
