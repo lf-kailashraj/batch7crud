a32b117bd5f497e4cae6b1d337b93f977a848baa
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-18 17:51:57 +0545

    added create method in UserServices

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/services/UserServices.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/services/UserServices.java
@@ -1,8 +1,21 @@
 package com.lftechnology.batch7crud.services;
 
+import com.lftechnology.batch7crud.dao.UserDao;
+import com.lftechnology.batch7crud.exception.DataException;
+import com.lftechnology.batch7crud.model.User;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by grishma on 1/18/16.
  */
 public class UserServices {
-    
+    List<User> userList = new ArrayList<User>();
+    public void create(String name, String username, String email, String password) throws DataException {
+        UserDao userDao = new UserDao();
+        userDao.create(name, username, email, password);
+    }
+
 }
