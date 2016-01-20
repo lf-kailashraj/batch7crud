7df45f7139fa3b34dd48d6bdd5f35ecdd1bce0eb
bishalshr <bishalshrestha@lftechnology.com>
2016-01-20 22:45:43 +0545

    major issue fixed

--- batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/CommonConstant.java
+++ batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/CommonConstant.java
@@ -7,6 +7,7 @@ public class CommonConstant {
   public static final String DELETE = "delete";
   public static final String INDEX_PAGE = "/WEB-INF/views/student/index.jsp";
   public static final String ERROR_PAGE = "/WEB-INF/views/error.jsp";
+  
   private CommonConstant() {
   }
 }
