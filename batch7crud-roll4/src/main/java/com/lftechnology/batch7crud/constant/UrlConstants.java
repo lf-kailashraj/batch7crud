13f7adb432986415cf97cc1fdb55336bda5e1786
pratishshr <vanroshr@gmail.com>
2016-01-21 13:53:52 +0545

    change in url constants

--- batch7crud-roll4/src/main/java/com/lftechnology/batch7crud/constant/UrlConstants.java
+++ batch7crud-roll4/src/main/java/com/lftechnology/batch7crud/constant/UrlConstants.java
@@ -1,15 +1,20 @@
 package com.lftechnology.batch7crud.constant;
 
+import java.io.File;
+
 /**
  * Created by pratishshr on 1/21/16.
  */
 public class UrlConstants {
-  public static final String URL_EMPLOYEE_LISTING_PAGE = "/WEB-INF/views/employees.jsp";
-  public static final String URL_EMPLOYEE_CREATE_PAGE = "/WEB-INF/views/create.jsp";
-  public static final String URL_EMPLOYEE_PROFILE_PAGE = "/WEB-INF/views/profile.jsp";
-  public static final String URL_EMPLOYEE_EDIT_PAGE = "/WEB-INF/views/edit.jsp";
+  public static final String SEPARATOR = File.separator;
+  public static final String VIEW = SEPARATOR + "WEB-INF" + SEPARATOR + "views" + SEPARATOR;
 
-  public static final String URL_ERROR_PAGE = "/WEB-INF/views/error.jsp";
+  public static final String URL_EMPLOYEE_LISTING_PAGE = VIEW + "employees.jsp";
+  public static final String URL_EMPLOYEE_CREATE_PAGE = VIEW + "create.jsp";
+  public static final String URL_EMPLOYEE_PROFILE_PAGE = VIEW + "profile.jsp";
+  public static final String URL_EMPLOYEE_EDIT_PAGE = VIEW + "edit.jsp";
+
+  public static final String URL_ERROR_PAGE = VIEW + "error.jsp";
 
   private UrlConstants() {
 
