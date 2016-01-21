ebb069372ce4ce65e45cd780b947afec7f379a7f
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-21 11:35:52 +0545

    refactored UserServices

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/services/UserServices.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/services/UserServices.java
@@ -4,18 +4,13 @@ import com.lftechnology.batch7crud.dao.UserDao;
 import com.lftechnology.batch7crud.exception.DataException;
 import com.lftechnology.batch7crud.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Created by grishma on 1/18/16.
+ * Created by Grishma Shrestha grishmashrestha@lftechnology.com on 1/18/16.
  */
 public class UserServices {
-  List<User> userList = new ArrayList<User>();
-  public Boolean create(User user) throws DataException {
-    UserDao userDao = new UserDao();
+  UserDao userDao = new UserDao();
+  public void create(User user) throws DataException {
     userDao.create(user);
-    return null;
   }
 
 }
