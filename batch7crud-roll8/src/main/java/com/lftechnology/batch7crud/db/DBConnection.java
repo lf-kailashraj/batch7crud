aa533f82fd712b90a2092aa0a428448ce7ffbff7
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-21 12:18:32 +0545

    logger added to DBConnection

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/db/DBConnection.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/db/DBConnection.java
@@ -8,23 +8,35 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Grishma Shrestha grishmashrestha@lftechnology.com on 1/18/16.
  */
 
 public class DBConnection {
+
+  private static final Logger LOGGER = Logger.getLogger(DBConnection.class.getName());
+
+  private DBConnection () {
+
+  }
+
   public static Connection getConnection() throws DataException {
     try {
       Context initContext = new InitialContext();
       Context envContext = (Context) initContext.lookup("java:/comp/env");
       DataSource dataSource = (DataSource) envContext.lookup("jdbc/employeemgt");
-      Connection conn = dataSource.getConnection();
+      Connection conn;
+      conn = dataSource.getConnection();
       return conn;
     } catch (SQLException e) {
-      throw new DataException();
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+      throw new DataException(e.getMessage());
     } catch (NamingException e) {
-      throw new DataException();
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+      throw new DataException(e.getMessage());
     }
   }
 }
