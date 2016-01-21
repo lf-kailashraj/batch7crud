f9122824350334d2bc81aa3508e911a8bfb61895
bishalshr <bishalshrestha@lftechnology.com>
2016-01-21 15:11:16 +0545

    constant page changed

--- batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/CommonConstant.java
+++ batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/CommonConstant.java
@@ -1,18 +1,23 @@
 package com.lftechnology.batch7crud.constant;
 
+import java.io.File;
+
 public class CommonConstant {
   public static final String MESSAGE = "message";
   public static final String CREATE = "create";
   public static final String EDIT = "edit";
   public static final String DELETE = "delete";
-  
-  public static final String INDEX_PAGE = "/WEB-INF/views/index.jsp";
-  public static final String ERROR_PAGE = "/WEB-INF/views/error.jsp";
+
+  public static final String SEPARATOR = File.separator;
+  public static final String VIEW = SEPARATOR + "WEB-INF" + SEPARATOR + "views" + SEPARATOR;
+  public static final String INDEX_PAGE = VIEW + "index.jsp";
+  public static final String ERROR_PAGE = VIEW + "error.jsp";
 
   public static final String PAGE_NUMBER = "page";
   public static final String NUMBER_OF_PAGES = "numberOfPages";
-  
+
   public static final String PAGE_NOT_FOUND = "Page Not Found";
+
   private CommonConstant() {
   }
 }
--- batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/StudentConstant.java
+++ batch7crud-roll3/src/main/java/com/lftechnology/batch7crud/constant/StudentConstant.java
@@ -1,6 +1,6 @@
 package com.lftechnology.batch7crud.constant;
 
-import java.io.File;
+import static com.lftechnology.batch7crud.constant.CommonConstant.*;
 
 public class StudentConstant {
   public static final String STUDENT = "student";
@@ -10,16 +10,13 @@ public class StudentConstant {
   public static final String ROLL = "roll";
   public static final String NAME = "name";
 
-  public static final String SEPARATOR = File.separator;
-  public static final String VIEW = SEPARATOR + "WEB-INF" + SEPARATOR + "views" + SEPARATOR + "students" + SEPARATOR;
-  public static final String EDIT_PAGE = VIEW + "edit.jsp";
-  public static final String CREATE_PAGE = VIEW + "create.jsp";
-  public static final String SHOW_PAGE = VIEW + "show.jsp";
-  public static final String LIST_PAGE = VIEW + "list.jsp";
+  public static final String STUDENTS = VIEW + "students" + SEPARATOR;
+  public static final String EDIT_PAGE = STUDENTS + "edit.jsp";
+  public static final String CREATE_PAGE = STUDENTS + "create.jsp";
+  public static final String SHOW_PAGE = STUDENTS + "show.jsp";
+  public static final String LIST_PAGE = STUDENTS + "list.jsp";
 
   public static final String INVALID_ROLL_MESSAGE = "Invalid Roll Number";
-  
- 
 
   private StudentConstant() {
   }
