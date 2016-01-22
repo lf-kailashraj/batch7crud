34450f69d19cada98dda6eebc5c84c27f1fea14c
Prajjwal Kandel <prajjwalkandel@lftechnology.com>
2016-01-22 13:53:34 +0545

    Too many concats in common constants handled

--- batch7crud-roll7/src/main/java/com/lftechnology/batch7crud/controller/CommonConstants.java
+++ batch7crud-roll7/src/main/java/com/lftechnology/batch7crud/controller/CommonConstants.java
@@ -10,12 +10,13 @@ public class CommonConstants {
   public static final String NEW_ENTRY = "newEntry";
   public static final String EDIT = "edit";
   public static final String DELETE = "delete";
-  public static final String STUDENTS_LIST_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "students.jsp";
-  public static final String NEW_ENTRY_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "newEntry.jsp";
-  public static final String EDIT_ENTRY_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "editEntry.jsp";
-  public static final String SHOW_STUDENT_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "show.jsp";
-  public static final String ERROR_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "error.jsp";
-  public static final String INDEX_VIEW = File.separator + "WEB-INF" + File.separator + "views" + File.separator + "index.jsp";
+  private static final String VIEW_BASE = File.separator + "WEB-INF" + File.separator + "views" + File.separator;
+  public static final String STUDENTS_LIST_VIEW = VIEW_BASE + "students.jsp";
+  public static final String NEW_ENTRY_VIEW = VIEW_BASE + "newEntry.jsp";
+  public static final String EDIT_ENTRY_VIEW = VIEW_BASE + "editEntry.jsp";
+  public static final String SHOW_STUDENT_VIEW = VIEW_BASE + "show.jsp";
+  public static final String ERROR_VIEW = VIEW_BASE + "error.jsp";
+  public static final String INDEX_VIEW = VIEW_BASE + "index.jsp";
   public static final int PAGE_SIZE = 3;
 
   private CommonConstants(){
