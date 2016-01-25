2f383593022f471bb712fd2f6239b7cca4f4990e
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-25 17:54:05 +0545

    methods added to Validator interface

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/validator/Validator.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/validator/Validator.java
@@ -1,7 +1,12 @@
 package com.lftechnology.batch7crud.validator;
 
+import java.util.Map;
+
 /**
  * Created by Grishma Shrestha grishmashrestha@lftechnology.com on 1/25/16.
  */
-public class Validator {
+public interface Validator <T> {
+  public T createObject(Map<String, String> input);
+
+  public Map<String, String> validate (T entity);
 }
