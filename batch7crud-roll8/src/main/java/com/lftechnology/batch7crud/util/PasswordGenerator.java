7814f3d7a1adfff8c5f9144f35634ef79181dcc4
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-01-29 11:26:52 +0545

    removed unused Logger from Password Generator

--- batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/util/PasswordGenerator.java
+++ batch7crud-roll8/src/main/java/com/lftechnology/batch7crud/util/PasswordGenerator.java
@@ -3,13 +3,11 @@ package com.lftechnology.batch7crud.util;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Logger;
 
 /**
  * Created by Grishma Shrestha grishmashrestha@lftechnology.com on 1/29/16.
  */
 public class PasswordGenerator {
-  private static final Logger LOGGER = Logger.getLogger(PasswordGenerator.class.getName());
   private PasswordGenerator() {
   }
 
