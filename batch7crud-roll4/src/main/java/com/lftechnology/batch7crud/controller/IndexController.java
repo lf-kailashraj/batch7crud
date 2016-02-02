9f00813dce02adf0bd1566e6f29ed87a5b1448cd
pratishshr <pratishshrestha@lftechnology.com>
2016-02-02 13:55:54 +0545

    finalized

--- batch7crud-roll4/src/main/java/com/lftechnology/batch7crud/controller/IndexController.java
+++ batch7crud-roll4/src/main/java/com/lftechnology/batch7crud/controller/IndexController.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import static com.lftechnology.batch7crud.constant.UrlConstants.URL_INDEX_PAGE;
 
 @WebServlet({ "/index" })
-
 public class IndexController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
